d40931ee21a5a18a3dbfd0d20495ec4c723d0799
Supriya <supriyasg2911@gmail.com>
2024-03-02 01:49:36 +0530

    first commit

---
+++ src/components/FooterComponent.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+
+const FooterComponent = () => {
+  return (
+    <div>
+      <footer className="footer">
+        <p className="text-center">
+          All copy rights reserved to Shravani Godse
+        </p>
+      </footer>
+    </div>
+  );
+};
+
+export default FooterComponent;
