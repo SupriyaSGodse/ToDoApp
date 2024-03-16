19eec90bfbb4d9d066bf0d6b7afbc398232f362f
Supriya <supriyasg2911@gmail.com>
2024-03-17 01:08:37 +0530

    Removed extra Todo Backend Directory

--- Todo_Backend/src/main/java/com/example/todo/repository/TodoRepository.java
+++
@@ -1,9 +0,0 @@
-package com.example.todo.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.example.todo.entity.Todo;
-
-public interface TodoRepository extends JpaRepository<Todo, Long>{
-
-}
